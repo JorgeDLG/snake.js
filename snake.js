b85911e11559e1ffa52e23579fb80acd75a0d4b1
dlag <idroj07@gmail.com>
2019-08-12 23:56:48 +0200

    canvas perfilado

--- snake.js
+++ snake.js
@@ -1,5 +1,6 @@
+// alert('test');
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
-
+// debugger;
 ctx.fillStyle = 'green';
-ctx.fillRect(10, 10, 100, 100);
+ctx.fillRect(5, 10, 350, 350);
